9264707608b46ad9da547535a62b00f7b7a3e237
Maria Pokryshkina <mariapokryshkina@Marias-MacBook-Air.local>
2024-03-03 23:02:06 +0300

    added component and styles

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -1,9 +1,82 @@
 import React from 'react'
 import styled from 'styled-components'
 
-export const Timeline = ({ items }) => {
-  // TODO: Replace me
-  return <StyledPlaceholder>your component here</StyledPlaceholder>
+const TimelineWrapper = styled.div`
+  color: #2E3438;
+  font-family: "Nunito Sans",-apple-system,".SFNSText-Regular","San Francisco",BlinkMacSystemFont,"Segoe UI","Helvetica Neue",Helvetica,Arial,sans-serif;
+  font-size: 16px;
+
+  .timeline-event {
+    padding: 10px;
+    margin-bottom: 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .container{
+    display:flex;
+    flex-direction: column;
+  }
+
+  .timeline-event-date {
+    margin-top: 5px;
+  }
+
+  .timeline-event-label {
+    margin-top: 5px;
+  }
+
+   .timeline-event-priority {
+    margin-top: 5px;
+  }
+
+  .timeline-event-description {
+    margin-top: 5px;
+  }
+
+   @media only screen and (max-width: 767px) { /* Mobile devices */
+    .timeline-event {
+      font-size: 14px;
+    }
+  }
+
+  @media only screen and (min-width: 768px) and (max-width: 991px) { /* Tablets */
+    .timeline-event {
+      font-size: 16px;
+    }
+  }
+
+  @media only screen and (min-width: 992px) { /* Desktop devices */
+    .timeline-event {
+      font-size: 18px;
+    }
+`;
+
+
+const Timeline = ({ items }) => {
+
+  const sortedItems = items.sort((a, b) => new Date(a.date) - new Date(b.date));
+  
+  return <StyledPlaceholder>
+    <TimelineWrapper>
+    <div className="timeline">
+      <h1 align="left"> Timeline Component  🗒️</h1>
+      <div className='container'>
+      {sortedItems.map((event, index) => (
+        <div key={index} className="timeline-event">
+          <div className="timeline-event-date"><b>Date: </b>{event.date} </div>
+          <div className="timeline-event-label"><b>Type: </b>{event.label || event.type}</div>
+          <div className="timeline-event-priority"><b>Priority: </b>{event.priority}</div>
+          <div className="timeline-event-description"><b>Description: </b>{event.description}</div>
+        </div>
+      ))}
+      <h1 align="left">Don't worry, we've got you covered 💆</h1>
+
+      </div>
+    </div>
+    </TimelineWrapper>
+    </StyledPlaceholder>
 }
 
 const StyledPlaceholder = styled.div`
@@ -15,3 +88,4 @@ const StyledPlaceholder = styled.div`
   color: #1b98f5;
   text-align: center;
 `
+export { Timeline };
